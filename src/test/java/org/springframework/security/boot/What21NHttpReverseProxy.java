4c713bb9e5a5dae50f0fff220bc1b8bddb0f7c06
wandl <hnxyhcwdl1003@163.com>
2019-12-27 16:37:38 +0800

    修改版权信息

--- src/test/java/org/springframework/security/boot/What21NHttpReverseProxy.java
+++ src/test/java/org/springframework/security/boot/What21NHttpReverseProxy.java
@@ -1,6 +1,6 @@
 package org.springframework.security.boot;
 /*
- * Copyright (c) 2018, vindell (https://github.com/vindell).
+ * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
