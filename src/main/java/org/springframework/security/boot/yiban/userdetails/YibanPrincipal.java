4f9b557bdf12af4a6b214acb5b0e48ecf280ce6a
wandl-6A72h <hiwepy@gmail.com>
2026-08-07 00:06:47 +0800

    refactor: 依赖版本号抽离到 properties + dependencyManagement

--- src/main/java/org/springframework/security/boot/yiban/userdetails/YibanPrincipal.java
+++ src/main/java/org/springframework/security/boot/yiban/userdetails/YibanPrincipal.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ * Copyright (c) 2018, hiwepy (https://github.com/easy-4-java).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -22,7 +22,7 @@ import org.springframework.security.core.GrantedAuthority;
 
 /**
  *      易班用户基本信息
- * @author ： <a href="https://github.com/hiwepy">wandl</a>
+ * @author ： <a href="https://github.com/easy-4-java">wandl</a>
  */
 @SuppressWarnings("serial")
 public class YibanPrincipal extends SecurityPrincipal {
--- src/test/java/org/springframework/security/boot/What21NHttpReverseProxy.java
+++ src/test/java/org/springframework/security/boot/What21NHttpReverseProxy.java
@@ -1,6 +1,6 @@
 package org.springframework.security.boot;
 /*
- * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ * Copyright (c) 2018, hiwepy (https://github.com/easy-4-java).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
