53d7e3c312bb3be7861b4532c7cc2b120ccba1e0
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:05:50 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/springframework/security/boot/yiban/authentication/YibanAuthorizationProcessingFilter.java
+++ src/main/java/org/springframework/security/boot/yiban/authentication/YibanAuthorizationProcessingFilter.java
@@ -46,7 +46,7 @@ import cn.yiban.open.Authorize;
 /**
  * Jwt授权 (authorization)过滤器
  * 
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class YibanAuthorizationProcessingFilter extends PostOnlyAuthenticationProcessingFilter {
 	
